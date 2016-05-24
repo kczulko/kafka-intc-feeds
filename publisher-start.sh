c5b20f64bec571acd750db6fd7ad2a9de29fd2a1
kczulko <karol.czulkowski@gmail.com>
2016-05-24 23:44:06 +0200

    publisher-start.sh comment fix

--- publisher-start.sh
+++ publisher-start.sh
@@ -5,7 +5,7 @@
 # -DinitialValue=<value> - initial stock value
 # -Dbrokers="localhost:9092"
 #
-# ./publisher-start.sh -DcompanyId=INTC -Dbrokers="localhost:9092,localhos:9093" -DinitialValue=30
+# ./publisher-start.sh -DcompanyId=INTC -Dbrokers="localhost:9092,localhost:9093" -DinitialValue=30
 #
 
 java $@ -jar producer/target/scala-2.11/producer-assembly-0.1-SNAPSHOT.jar
