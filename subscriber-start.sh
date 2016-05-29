01a7ae5ae950535664d4db6ec453e860cc246de7
kczulko <karol.czulkowski@gmail.com>
2016-05-29 20:10:20 +0200

    Example fix

--- subscriber-start.sh
+++ subscriber-start.sh
@@ -6,7 +6,7 @@
 # -Dbrokers=<list_of_brokers> - eg. localhost:9092
 # -DstockThreshold=<threshold> - eg. 30.5
 #
-# ./subscriber-start.sh -DcompanyId=INTC -Dbrokers="localhost:9092,localhost:9093" -DstockTreshold=30.5
+# ./subscriber-start.sh -DcompanyId=INTC -Dbrokers="localhost:9092,localhost:9093" -DstockThreshold=30.5
 #
 
 java $@ -jar consumer/target/scala-2.11/consumer-assembly-0.1-SNAPSHOT.jar
